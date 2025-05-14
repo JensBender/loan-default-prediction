ef810116eeb5798d07ad226f14077bb93ec39cec
Jens Bender <jens.bender.001@gmail.com>
2025-05-14 14:09:58 +0200

    In app.py: Add profession input as dropdown with placeholder list for predefined professions

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
 import gradio as gr
 
-def predict_loan_default(income, age, experience):
+professions = ["profession_01", "profession_02", "profession_03", "profession_04", "profession_05"]
+
+
+def predict_loan_default(income, age, experience, profession):
     return f"""
     Income: {income}
     Age: {age}
     Experience: {experience}
+    Profession: {profession}
     
     To Do: Implement loan default prediction model...
     """
+
 
 demo = gr.Interface(
     fn=predict_loan_default,
@@ -15,6 +20,7 @@ demo = gr.Interface(
         gr.Number(label="Income"), 
         gr.Number(label="Age"),
         gr.Number(label="Experience"),
+        gr.Dropdown(label="Profession", choices=professions)
         ],
     outputs=gr.Textbox(),
     title="Loan Default Prediction",
