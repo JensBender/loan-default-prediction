1353590a58aa7656d84a96d4867f96945784ccaf
Jens Bender <jens.bender.001@gmail.com>
2025-05-14 13:56:49 +0200

    In app.py: Add experience as input and update predict_loan_default function

--- app/app.py
+++ app/app.py
@@ -1,13 +1,20 @@
 import gradio as gr
 
-def predict_loan_default(income, age):
-    return f"Income: {income}\nAge: {age}\n\nTo Do: Implement loan default prediction model..."
+def predict_loan_default(income, age, experience):
+    return f"""
+    Income: {income}
+    Age: {age}
+    Experience: {experience}
+    
+    To Do: Implement loan default prediction model...
+    """
 
 demo = gr.Interface(
     fn=predict_loan_default,
     inputs=[
         gr.Number(label="Income"), 
         gr.Number(label="Age"),
+        gr.Number(label="Experience"),
         ],
     outputs=gr.Textbox(),
     title="Loan Default Prediction",
