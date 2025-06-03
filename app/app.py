20cf978fb0078be6571418bc9414f4febb235c6b
Jens Bender <jens.bender.001@gmail.com>
2025-06-03 11:47:34 +0200

    In app/app.py > loan_default_prediction function: Add predicted probability and information on optimized decision threshold in prediction_text

--- app/app.py
+++ app/app.py
@@ -203,7 +203,7 @@ def predict_loan_default(income, age, experience, married, house_ownership, car_
 
         # Create prediction text
         prediction_label_map = {0: "No Default", 1: "Default"}
-        prediction_text = f"{prediction_label_map[pred]}"
+        prediction_text = f"{prediction_label_map[pred]} ({pred_proba[1] * 100:.0f}% Probability, {optimized_threshold * 100:.0f}% Threshold)"
 
         return pred_proba_dict, prediction_text, pipeline_input_df, pred_proba
     
