9012527d42a5b5ae03d82509e5fcf69fbe1c0036
Jens Bender <jens.bender.001@gmail.com>
2025-06-05 14:12:01 +0200

    In app/app.py: Add input validation tests for various scenarios of 1, 2, or 3 invalid numerical or string types

--- app/app.py
+++ app/app.py
@@ -333,9 +333,17 @@ with gr.Blocks(css=custom_css) as app:
 
 
 if __name__ == "__main__":
-    # Input validation tests
+    # --- Input validation tests ---
+    # 1 invalid numerical type
     print(predict_loan_default(50000, "five", 10, "Married", "Owned", "Yes", "Engineer", "Delhi", "Delhi", 5, 12)[0])
+    # 2 invalid numerical types
     print(predict_loan_default("50K", "five", 10, "Married", "Owned", "Yes", "Engineer", "Delhi", "Delhi", 5, 12)[0])
+    # 1 invalid string type
+    print(predict_loan_default(50000, 30, 10, "Single", "Owned", 1, "Engineer", "Delhi", "Delhi", 5, 12)[0])
+    # 2 invalid string types
+    print(predict_loan_default(50000, 30, 10, "Single", "Owned", False, 12345, "Delhi", "Delhi", 5, 12)[0])
+    # 3 invalid string types
+    print(predict_loan_default(50000, 30, 10, "Single", "Owned", "Yes", 12345, 12345, 12345, 5, 12)[0])
     
     # Launch the app
     app.launch()
