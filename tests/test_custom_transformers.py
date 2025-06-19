18745a0a126d09c2deaebb73c22ff9fa11abc2ac
Jens Bender <jens.bender.001@gmail.com>
2025-06-19 14:41:49 +0200

    In tests directory: Add test_custom_transformers.py with imports

---
+++ tests/test_custom_transformers.py
@@ -0,0 +1,19 @@
+# Standard library imports
+import os
+import sys
+import warnings
+
+# Third-party library imports
+import pytest
+
+# Suppress deprecation warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)  
+
+# Add the parent directory to the path (for local imports)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))  
+
+# Local imports
+from app.custom_transformers import (
+   MissingValueChecker 
+)
+
