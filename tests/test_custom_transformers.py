10fa5e50fe53be705be491ccb6642133e121d2e6
Jens Bender <jens.bender.001@gmail.com>
2025-06-19 15:02:43 +0200

    In tests/test_custom_transformers.py: Add test_MissingValueChecker_instantiation function

--- tests/test_custom_transformers.py
+++ tests/test_custom_transformers.py
@@ -5,6 +5,7 @@ import warnings
 
 # Third-party library imports
 import pytest
+from sklearn.base import BaseEstimator, TransformerMixin
 
 # Suppress deprecation warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)  
@@ -17,3 +18,14 @@ from app.custom_transformers import (
    MissingValueChecker 
 )
 
+
+# --- Test MissingValueChecker class ---
+# Class instantiation 
+def test_MissingValueChecker_instantiation():
+    critical_features=['feature1', 'feature2']
+    non_critical_features=['feature3', 'feature4']
+    transformer = MissingValueChecker(critical_features, non_critical_features)
+    assert isinstance(transformer, BaseEstimator)
+    assert isinstance(transformer, TransformerMixin)
+    assert transformer.critical_features == critical_features
+    assert transformer.non_critical_features == non_critical_features
