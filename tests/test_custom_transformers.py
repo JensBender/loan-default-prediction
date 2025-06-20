29e13bf6837082794ae50d5501376e00fb687739
Jens Bender <jens.bender.001@gmail.com>
2025-06-20 10:11:27 +0200

    In tests/test_custom_transformers.py > Test MissingValueChecker class: Add test_MissingValueChecker_sklean_compatibility function using sklearn's check_estimator

--- tests/test_custom_transformers.py
+++ tests/test_custom_transformers.py
@@ -5,6 +5,7 @@ import warnings
 
 # Third-party library imports
 import pytest
+from sklearn.utils.estimator_checks import check_estimator
 from sklearn.base import BaseEstimator, TransformerMixin
 
 # Suppress deprecation warnings
@@ -22,10 +23,18 @@ from app.custom_transformers import (
 # --- Test MissingValueChecker class ---
 # Class instantiation 
 def test_MissingValueChecker_instantiation():
-    critical_features=['feature1', 'feature2']
-    non_critical_features=['feature3', 'feature4']
-    transformer = MissingValueChecker(critical_features, non_critical_features)
-    assert isinstance(transformer, BaseEstimator)
-    assert isinstance(transformer, TransformerMixin)
-    assert transformer.critical_features == critical_features
-    assert transformer.non_critical_features == non_critical_features
+   critical_features = ["feature_1", "feature_2"]
+   non_critical_features = ["feature_3", "feature_4"]
+   transformer = MissingValueChecker(critical_features, non_critical_features)
+   assert isinstance(transformer, BaseEstimator)
+   assert isinstance(transformer, TransformerMixin)
+   assert transformer.critical_features == critical_features
+   assert transformer.non_critical_features == non_critical_features
+
+
+# Scikit-learn compatibility
+def test_MissingValueChecker_sklearn_compatibility():
+   critical_features = ["feature_1", "feature_2"]
+   non_critical_features = ["feature_3", "feature_4"]
+   transformer = MissingValueChecker(critical_features, non_critical_features)
+   return check_estimator(transformer)
