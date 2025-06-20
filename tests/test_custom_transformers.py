8d9165ffb7f284cb64b5f67012663f1830399381
Jens Bender <jens.bender.001@gmail.com>
2025-06-20 14:39:06 +0200

    In tests/test_custom_transformers.py: Update pytest.fixture sample_df to mirror real data

--- tests/test_custom_transformers.py
+++ tests/test_custom_transformers.py
@@ -21,14 +21,23 @@ from app.custom_transformers import (
 )
 
 
-# Fixture to provide sample pandas DataFrame for testing
+# Fixture to provide sample pandas DataFrame that mirrors the real data for testing
 @pytest.fixture
 def sample_df():
-    return pd.DataFrame({
-        "feature_1": [1, 2, 3],
-        "feature_2": [4, 5, 6],
-        "feature_3": [7, 8, 9]
-    })
+   return pd.DataFrame({
+      "income": [9121364, 2636544, 9470213, 6558967, 6245331, None],
+      "age": [70, 39, 41, 41, 65, 64],
+      "experience": [18, 0, 5, 10, 6, 1],
+      "married": ["single", "single", "single", "married", "single", "single"],
+      "house_ownership": ["rented", "rented", "norent_noown", "rented", "rented", "owned"],
+      "car_ownership": ["no", "no", "yes", "no", "no", "no"],
+      "profession": ["Artist", "Computer_hardware_engineer", "Web_designer", "Comedian", 
+                     "Financial_Analyst", "Statistician"],
+      "city": ["Sikar", "Vellore", "Bidar", "Bongaigaon", "Eluru[25]", "Danapur"],
+      "state": ["Rajasthan", "Tamil_Nadu", "Karnataka", "Assam", "Andhra_Pradesh", "Bihar"],
+      "current_job_yrs": [3, 0, 5, 10, 6, 1],
+      "current_house_yrs": [11, 11, 13, 12, 12, 12],
+   })
 
 
 # --- Test MissingValueChecker class ---
