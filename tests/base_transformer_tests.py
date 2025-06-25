65014c5b054cbb7f2fb439d918e116dbef72b063
Jens Bender <jens.bender.001@gmail.com>
2025-06-25 14:59:07 +0200

    In tests/base_transformer_tests.py: Add BaseTransformerTests class method .test_fit_learns_attributes()

--- tests/base_transformer_tests.py
+++ tests/base_transformer_tests.py
@@ -16,3 +16,13 @@ class BaseTransformerTests:
         X = X_input.copy()
         fitted_transformer = transformer.fit(X)
         assert fitted_transformer is transformer
+
+    # Ensure .fit() stores learned attributes correctly (feature number and names of the input DataFrame) 
+    @pytest.mark.unit
+    def test_fit_learns_attributes(self, transformer, X_input, learned_attributes):
+        X = X_input.copy()
+        transformer.fit(X)  
+        for attribute in learned_attributes:
+            assert hasattr(transformer, attribute)
+        assert transformer.n_features_in_ == X.shape[1]
+        assert transformer.feature_names_in_ == X.columns.tolist()
