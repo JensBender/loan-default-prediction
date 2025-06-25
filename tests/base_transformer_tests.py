f3afc1607a166d319dd4f8b4df30b42948808594
Jens Bender <jens.bender.001@gmail.com>
2025-06-25 14:41:15 +0200

    In tests/base_transformer_tests.py: Add BaseTransformerTests class method .test_fit_returns_self()

--- tests/base_transformer_tests.py
+++ tests/base_transformer_tests.py
@@ -9,3 +9,10 @@ class BaseTransformerTests:
     def test_instantiation(self, transformer):
         assert isinstance(transformer, BaseEstimator)
         assert isinstance(transformer, TransformerMixin)
+
+    # Ensure .fit() returns the instance (self)
+    @pytest.mark.unit
+    def test_fit_returns_self(self, transformer, X_input):
+        X = X_input.copy()
+        fitted_transformer = transformer.fit(X)
+        assert fitted_transformer is transformer
