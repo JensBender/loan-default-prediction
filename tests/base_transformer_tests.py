92793520a8c5af58a4109dcb7c7d5a55fee6c9c2
Jens Bender <jens.bender.001@gmail.com>
2025-06-25 11:31:08 +0200

    In tests directory: Rename test_transformer_base.py to base_transformer_tests.py and rename class TransformerBaseTests to BaseTransformerTests

--- tests/base_transformer_tests.py
+++ tests/base_transformer_tests.py
@@ -3,7 +3,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 # Base tests for custom sklearn transformer classes (that individual test classes can inherit from)
-class TransformerBaseTests:
+class BaseTransformerTests:
     # Class instantiation 
     @pytest.mark.unit
     def test_instantiation(self, transformer):
