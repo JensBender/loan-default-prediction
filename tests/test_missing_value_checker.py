565ea708e63dc7a0ec5c6973350af42eda292534
Jens Bender <jens.bender.001@gmail.com>
2025-06-27 10:13:12 +0200

    In tests/test_missing_value_checker.py: Add fixture to create X input DataFrame for use in tests

--- tests/test_missing_value_checker.py
+++ tests/test_missing_value_checker.py
@@ -4,6 +4,7 @@ import sys
 
 # Third-party library imports
 import pytest
+import pandas as pd
 
 # Add the parent directory to the path (for local imports)
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -24,6 +25,22 @@ def transformer():
     )
 
 # Fixture to create X input DataFrame for use in tests
+@pytest.fixture
+def X_input():
+    return pd.DataFrame({
+        "income": [9121364, 2636544, 9470213, 6558967, 6245331, None],
+        "age": [70, 39, 41, 41, 65, 64],
+        "experience": [18, 0, 5, 10, 6, 1],
+        "married": ["single", "single", "single", "married", "single", "single"],
+        "house_ownership": ["rented", "rented", "norent_noown", "rented", "rented", "owned"],
+        "car_ownership": ["no", "no", "yes", "no", "no", "no"],
+        "profession": ["Artist", "Computer_hardware_engineer", "Web_designer", "Comedian", 
+                       "Financial_Analyst", "Statistician"],
+        "city": ["Sikar", "Vellore", "Bidar", "Bongaigaon", "Eluru[25]", "Danapur"],
+        "state": ["Rajasthan", "Tamil_Nadu", "Karnataka", "Assam", "Andhra_Pradesh", "Bihar"],
+        "current_job_yrs": [3, 0, 5, 10, 6, 1],
+        "current_house_yrs": [11, 11, 13, 12, 12, 12],
+    })
 
 # --- TestMissingValueChecker class ---
 # Inherits from BaseTransformerTests which adds the following tests:
