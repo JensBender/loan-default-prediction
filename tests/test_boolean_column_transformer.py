23438ee4b66b8601175fec25ca63e701dd67783f
Jens Bender <jens.bender.001@gmail.com>
2025-07-05 14:26:40 +0200

    In tests/test_boolean_column_transformer.py: Add X_input fixture to create input DataFrame for use in tests

--- tests/test_boolean_column_transformer.py
+++ tests/test_boolean_column_transformer.py
@@ -21,6 +21,24 @@ from tests.base_transformer_tests import BaseTransformerTests
 def transformer():
     return BooleanColumnTransformer(boolean_column_mappings=BOOLEAN_COLUMN_MAPPINGS)
 
+# Fixture to create X input DataFrame for use in tests
+@pytest.fixture
+def X_input():
+    return pd.DataFrame({
+        "income": [9121364, 2636544, 9470213, 6558967, 6245331, 154867],
+        "age": [70, 39, 41, 41, 65, 64],
+        "experience": [18, 0, 5, 10, 6, 1],
+        "married": ["single", "single", "single", "married", "single", "single"],
+        "house_ownership": ["rented", "rented", "norent_noown", "rented", "rented", "owned"],
+        "car_ownership": ["no", "no", "yes", "no", "no", "no"],
+        "profession": ["artist", "computer_hardware_engineer", "web_designer", "comedian", 
+                       "financial_analyst", "statistician"],
+        "city": ["sikar", "vellore", "bidar", "bongaigaon", "eluru[25]", "danapur"],
+        "state": ["rajasthan", "tamil_nadu", "karnataka", "assam", "andhra_pradesh", "bihar"],
+        "current_job_yrs": [3, 0, 5, 10, 6, 1],
+        "current_house_yrs": [11, 11, 13, 12, 12, 12],
+    })
+
 
 # --- TestBooleanColumnTransformer class ---
 # Inherits from BaseTransformerTests which adds the following tests:
