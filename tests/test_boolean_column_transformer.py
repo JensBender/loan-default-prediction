144722f44763d01f785bb03c6c1ba113e982a3fd
Jens Bender <jens.bender.001@gmail.com>
2025-07-05 14:21:55 +0200

    In tests/test_boolean_column_transformer.py: Add transformer fixture to instantiate BooleanColumnTransformer class and class TestBooleanColumnTransformer that inherits from BaseTransformerTests

--- tests/test_boolean_column_transformer.py
+++ tests/test_boolean_column_transformer.py
@@ -14,3 +14,26 @@ from app.custom_transformers import BooleanColumnTransformer
 from app.global_constants import BOOLEAN_COLUMN_MAPPINGS
 from tests.base_transformer_tests import BaseTransformerTests
 
+
+# --- Fixtures ---
+# Fixture to instantiate BooleanColumnTransformer class for use in tests
+@pytest.fixture
+def transformer():
+    return BooleanColumnTransformer(boolean_column_mappings=BOOLEAN_COLUMN_MAPPINGS)
+
+
+# --- TestBooleanColumnTransformer class ---
+# Inherits from BaseTransformerTests which adds the following tests:
+# .test_instantiation()
+# .test_fit_returns_self()
+# .test_fit_learns_attributes()
+# .test_instance_can_be_cloned()
+# .test_fit_transform_equivalence()
+# .test_transform_does_not_modify_input_df()
+# .test_instance_can_be_pickled()
+# .test_fit_raises_type_error_for_invalid_input()
+# .test_transform_raises_not_fitted_error_if_unfitted()
+# .test_transform_raises_type_error_for_invalid_input()
+# .test_transform_raises_value_error_for_wrong_column_order()
+class TestBooleanColumnTransformer(BaseTransformerTests):
+    pass
