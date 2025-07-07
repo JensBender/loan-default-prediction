3ae82b5c2416e2c0a876ea80f57e2fce77de0b0b
Jens Bender <jens.bender.001@gmail.com>
2025-07-07 08:31:13 +0200

    In tests/test_boolean_column_transformer.py: Add TestBooleanColumnTransformer class method .test_instantiation()

--- tests/test_boolean_column_transformer.py
+++ tests/test_boolean_column_transformer.py
@@ -54,4 +54,10 @@ def X_input():
 # .test_transform_raises_type_error_for_invalid_input()
 # .test_transform_raises_value_error_for_wrong_column_order()
 class TestBooleanColumnTransformer(BaseTransformerTests):
-    pass
+    # Class instantiation 
+    def test_instantiation(self, transformer):
+        # First, run the .test_instantiation() method from the parent class BaseTransformerTests
+        super().test_instantiation(transformer)
+        # Then, add assertions specific to the BooleanColumnTransformer class
+        assert isinstance(transformer, BooleanColumnTransformer)
+        assert transformer.boolean_column_mappings == BOOLEAN_COLUMN_MAPPINGS
