0748269a48bd02cc72ae39208d5e8551e3a4404d
Jens Bender <jens.bender.001@gmail.com>
2025-07-07 14:34:20 +0200

    In tests/test_boolean_column_transformer.py: Add TestBooleanColumnTransformer class method .test_transform_converts_categorical_columns_to_boolean()

--- tests/test_boolean_column_transformer.py
+++ tests/test_boolean_column_transformer.py
@@ -5,6 +5,7 @@ import sys
 # Third-party library imports
 import pytest
 import pandas as pd
+from pandas.testing import assert_frame_equal
 import numpy as np
 
 # Add the parent directory to the path (for local imports)
@@ -153,3 +154,27 @@ class TestBooleanColumnTransformer(BaseTransformerTests):
         assert transformer.n_features_in_ == X.shape[1]
         assert transformer.feature_names_in_ == X.columns.tolist()
     
+    # Ensure .transform() successfully converts categorical string labels to boolean
+    @pytest.mark.unit
+    def test_transform_converts_categorical_columns_to_boolean(self, transformer, X_input):
+        X = X_input.copy()
+        # Fit and transform
+        transformer.fit(X)
+        X_transformed = transformer.transform(X)
+        # Expected output
+        expected_X_transformed = pd.DataFrame({
+            "income": [9121364, 2636544, 9470213, 6558967, 6245331, 154867],
+            "age": [70, 39, 41, 41, 65, 64],
+            "experience": [18, 0, 5, 10, 6, 1],
+            "married": [False, False, False, True, False, False],
+            "house_ownership": ["rented", "rented", "norent_noown", "rented", "rented", "owned"],
+            "car_ownership": [False, False, True, False, False, False],
+            "profession": ["artist", "computer_hardware_engineer", "web_designer", "comedian", 
+                        "financial_analyst", "statistician"],
+            "city": ["sikar", "vellore", "bidar", "bongaigaon", "eluru[25]", "danapur"],
+            "state": ["rajasthan", "tamil_nadu", "karnataka", "assam", "andhra_pradesh", "bihar"],
+            "current_job_yrs": [3, 0, 5, 10, 6, 1],
+            "current_house_yrs": [11, 11, 13, 12, 12, 12],
+        })
+        # Ensure actual and expected output DataFrames are identical
+        assert_frame_equal(X_transformed, expected_X_transformed)
