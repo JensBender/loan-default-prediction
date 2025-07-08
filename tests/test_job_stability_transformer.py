4eb4b4a368321dd8d4ce9a1a1ba1309573914a34
Jens Bender <jens.bender.001@gmail.com>
2025-07-08 13:35:21 +0200

    In tests/test_job_stability_transformer.py: Add transformer fixture that instantiates JobStabilityTransformer class and X_input features that creates example DataFrame for use in tests

--- tests/test_job_stability_transformer.py
+++ tests/test_job_stability_transformer.py
@@ -13,3 +13,45 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from app.custom_transformers import JobStabilityTransformer
 from app.global_constants import JOB_STABILITY_MAP
 from tests.base_transformer_tests import BaseTransformerTests
+
+
+# --- Fixtures ---
+# Fixture to instantiate JobStabilityTransformer class for use in tests
+@pytest.fixture
+def transformer():
+    return JobStabilityTransformer(job_stability_map=JOB_STABILITY_MAP)
+
+# Fixture to create X input DataFrame for use in tests
+@pytest.fixture
+def X_input():
+    return pd.DataFrame({
+        "income": [9121364, 2636544, 9470213, 6558967, 6245331, 154867],
+        "age": [70, 39, 41, 41, 65, 64],
+        "experience": [18, 0, 5, 10, 6, 1],
+        "married": [False, False, False, True, False, False],
+        "house_ownership": ["rented", "rented", "norent_noown", "rented", "rented", "owned"],
+        "car_ownership": [False, False, True, False, False, False],
+        "profession": ["artist", "computer_hardware_engineer", "web_designer", "comedian", 
+                       "financial_analyst", "statistician"],
+        "city": ["sikar", "vellore", "bidar", "bongaigaon", "eluru[25]", "danapur"],
+        "state": ["rajasthan", "tamil_nadu", "karnataka", "assam", "andhra_pradesh", "bihar"],
+        "current_job_yrs": [3, 0, 5, 10, 6, 1],
+        "current_house_yrs": [11, 11, 13, 12, 12, 12],
+    })
+
+
+# --- TestJobStabilityTransformer class ---
+# Inherits from BaseTransformerTests which adds the following tests:
+# .test_instantiation()
+# .test_fit_returns_self()
+# .test_fit_learns_attributes()
+# .test_instance_can_be_cloned()
+# .test_fit_transform_equivalence()
+# .test_transform_does_not_modify_input_df()
+# .test_instance_can_be_pickled()
+# .test_fit_raises_type_error_for_invalid_input()
+# .test_transform_raises_not_fitted_error_if_unfitted()
+# .test_transform_raises_type_error_for_invalid_input()
+# .test_transform_raises_value_error_for_wrong_column_order()
+class TestJobStabilityTransformer(BaseTransformerTests):
+    pass
