313608707c1d22348518353cd4a6dcb640688577
Jens Bender <jens.bender.001@gmail.com>
2025-07-09 15:45:31 +0200

    In app/custom_transformers.py > JobStabilityTransformer class: Update check for all values in 'profession' column are strings or missing values during .transform() again

--- app/custom_transformers.py
+++ app/custom_transformers.py
@@ -261,9 +261,7 @@ class JobStabilityTransformer(BaseEstimator, TransformerMixin):
             raise ValueError("Feature names and feature order of input X must be the same as during .fit().")      
 
         # Ensure all values in "profession" column are strings (or missing values)
-        if X["profession"].apply(lambda x: isinstance(x, (list, tuple, dict, set))).any():
-            raise TypeError("All values in 'profession' column must be strings or missing values.")
-        if not X["profession"].apply(lambda x: isinstance(x, str) or pd.isna(x)).all():
+        if not X["profession"].map(type).isin({str, type(None), float}).all():  # np.nan is a float, so must allow
             raise TypeError("All values in 'profession' column must be strings or missing values.")
 
         # Create job stability column by mapping professions to job stability tiers (default to "moderate" for unknown professions)
