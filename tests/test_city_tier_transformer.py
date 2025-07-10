5dbcd87c988d31c0b283e54bc2e7072634c51702
Jens Bender <jens.bender.001@gmail.com>
2025-07-10 13:00:26 +0200

    In tests/test_city_tier_transformer.py: Add transformer fixture, X_input fixture, and blueprint for TestCityTierTransformer class that inherits from BaseTransformerTests

--- tests/test_city_tier_transformer.py
+++ tests/test_city_tier_transformer.py
@@ -16,3 +16,45 @@ from app.custom_transformers import CityTierTransformer
 from app.global_constants import CITY_TIER_MAP
 from tests.base_transformer_tests import BaseTransformerTests
 
+
+# --- Fixtures ---
+# Fixture to instantiate JobStabilityTransformer class for use in tests
+@pytest.fixture
+def transformer():
+    return CityTierTransformer(city_tier_map=CITY_TIER_MAP)
+
+# Fixture to create X input DataFrame for use in tests
+@pytest.fixture
+def X_input():
+    return pd.DataFrame({
+        "income": [9121364, 2636544, 9470213, 6558967, 6245331, 154867],
+        "age": [70, 39, 41, 41, 65, 64],
+        "experience": [18, 0, 5, 10, 6, 1],
+        "married": [False, False, False, True, False, False],
+        "house_ownership": ["rented", "rented", "norent_noown", "rented", "rented", "owned"],
+        "car_ownership": [False, False, True, False, False, False],
+        "profession": ["artist", "computer_hardware_engineer", "web_designer", "comedian", 
+                       "financial_analyst", "statistician"],
+        "city": ["sikar", "vellore", "bidar", "bongaigaon", "eluru[25]", "danapur"],
+        "state": ["rajasthan", "tamil_nadu", "karnataka", "assam", "andhra_pradesh", "bihar"],
+        "current_job_yrs": [3, 0, 5, 10, 6, 1],
+        "current_house_yrs": [11, 11, 13, 12, 12, 12],
+        "job_stability": ["variable", "moderate", "variable", "variable", "moderate", "moderate"],
+    })
+
+
+# --- TestCityTierTransformer class ---
+# Inherits from BaseTransformerTests which adds the following tests:
+# .test_instantiation()
+# .test_fit_returns_self()
+# .test_fit_learns_attributes()
+# .test_instance_can_be_cloned()
+# .test_fit_transform_equivalence()
+# .test_transform_does_not_modify_input_df()
+# .test_instance_can_be_pickled()
+# .test_fit_raises_type_error_for_invalid_input()
+# .test_transform_raises_not_fitted_error_if_unfitted()
+# .test_transform_raises_type_error_for_invalid_input()
+# .test_transform_raises_value_error_for_wrong_column_order()
+class TestCityTierTransformer(BaseTransformerTests):
+    pass
