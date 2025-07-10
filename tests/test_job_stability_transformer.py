77925dbed833b117f4f50cf784c6d1d293442fb3
Jens Bender <jens.bender.001@gmail.com>
2025-07-10 09:05:39 +0200

    In tests/test_job_stability_transformer.py: Add test case for a set in parametrize of .test_transform_handles_empty_df()

--- tests/test_job_stability_transformer.py
+++ tests/test_job_stability_transformer.py
@@ -188,3 +188,19 @@ class TestJobStabilityTransformer(BaseTransformerTests):
         with pytest.raises(TypeError, match=expected_error_message):
             transformer.transform(X_with_invalid_profession_value)  
     
+    # Ensure .transform() correctly handles an empty DataFrame
+    @pytest.mark.unit
+    def test_transform_handles_empty_df(self, transformer, X_input):
+        X = X_input.copy()
+        # Create empty DataFrame
+        input_columns = X.columns.tolist()
+        X_empty = pd.DataFrame(columns=input_columns)
+        # Fit on "full" DataFrame
+        transformer.fit(X)
+        # Transform on empty Dataframe     
+        X_transformed = transformer.transform(X_empty)
+        # Create expected output
+        expected_output_columns = input_columns + ["job_stability"]
+        expected_X_transformed = pd.DataFrame(columns=expected_output_columns)
+        # Ensure actual and expected output DataFrames are identical
+        assert_frame_equal(X_transformed, expected_X_transformed)
