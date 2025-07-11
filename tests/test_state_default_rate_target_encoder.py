24b5b6eef5cef441607ff08e7782d38868fd7035
Jens Bender <jens.bender.001@gmail.com>
2025-07-11 12:04:59 +0200

    In tests/test_state_default_rate_target_encoder.py: Add fixtures for transformer, X_input, and y_input

--- tests/test_state_default_rate_target_encoder.py
+++ tests/test_state_default_rate_target_encoder.py
@@ -15,3 +15,34 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from app.custom_transformers import StateDefaultRateTargetEncoder
 from tests.base_transformer_tests import BaseTransformerTests
 
+
+# --- Fixtures ---
+# Fixture to instantiate JobStabilityTransformer class for use in tests
+@pytest.fixture
+def transformer():
+    return StateDefaultRateTargetEncoder()
+
+# Fixture to create X input DataFrame for use in tests
+@pytest.fixture
+def X_input():
+    return pd.DataFrame({
+        "income": [9121364, 2636544, 9470213, 6558967, 6245331, 154867],
+        "age": [70, 39, 41, 41, 65, 64],
+        "experience": [18, 0, 5, 10, 6, 1],
+        "married": [False, False, False, True, False, False],
+        "house_ownership": ["rented", "rented", "norent_noown", "rented", "rented", "owned"],
+        "car_ownership": [False, False, True, False, False, False],
+        "profession": ["artist", "computer_hardware_engineer", "web_designer", "comedian", 
+                       "financial_analyst", "statistician"],
+        "city": ["sikar", "vellore", "bidar", "bongaigaon", "eluru[25]", "danapur"],
+        "state": ["rajasthan", "tamil_nadu", "karnataka", "assam", "andhra_pradesh", "bihar"],
+        "current_job_yrs": [3, 0, 5, 10, 6, 1],
+        "current_house_yrs": [11, 11, 13, 12, 12, 12],
+        "job_stability": ["variable", "moderate", "variable", "variable", "moderate", "moderate"],
+        "city_tier": ["unknown", "unknown", "unknown", "unknown", "unknown", "tier_3"],
+    })
+
+# Fixture to create y input Series for use in tests
+@pytest.fixture
+def y_input():
+    return pd.Series([0, 1, 0, 0, 1, 0])
