0b566fba10694e719a7d13f99bcfee39d65b2876
Jens Bender <jens.bender.001@gmail.com>
2025-07-15 11:46:48 +0200

    In tests directory: Add documentation comment for two new methods inherited from BaseTransformerTests to all child classes

--- tests/test_feature_selector.py
+++ tests/test_feature_selector.py
@@ -57,7 +57,9 @@ def X_input():
 # .test_fit_raises_type_error_for_invalid_input()
 # .test_transform_raises_not_fitted_error_if_unfitted()
 # .test_transform_raises_type_error_for_invalid_input()
+# .test_transform_raises_value_error_for_extra_column()
 # .test_transform_raises_value_error_for_wrong_column_order()
+# .test_transform_preserves_df_index()
 class TestFeatureSelector(BaseTransformerTests):
     # Class instantiation 
     @pytest.mark.unit
