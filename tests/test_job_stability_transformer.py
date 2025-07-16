93ef892e6dfcaffa714c38e412e8a6c66c20fefd
Jens Bender <jens.bender.001@gmail.com>
2025-07-16 12:26:03 +0200

    In tests/test_job_stability_transformer.py: Remove .test_transform_assigns_moderate_to_unmapped_professions(), .test_transform_assigns_moderate_to_missing_professions() and .test_transform_raises_type_error_for_invalid_profession()

--- tests/test_job_stability_transformer.py
+++ tests/test_job_stability_transformer.py
@@ -219,41 +219,6 @@ class TestJobStabilityTransformer(BaseTransformerTests):
         with pytest.raises(CategoricalLabelError, match=expected_error_message):
             transformer.transform(X_with_unknown_profession)
 
-    # Ensure .transform() assigns "moderate" job stability to professions not in job_stability_map 
-    @pytest.mark.unit
-    def test_transform_assigns_moderate_to_unmapped_professions(self, transformer):
-        X_with_unmapped_professions = pd.DataFrame({
-            "profession": ["artist", "unmapped_profession_1", "web_designer", "unmapped_profession_2"]
-        })      
-        # Fit and transform
-        transformer.fit(X_with_unmapped_professions)
-        X_transformed = transformer.transform(X_with_unmapped_professions)
-        # Create expected output
-        expected_X_transformed = pd.DataFrame({
-            "profession": ["artist", "unmapped_profession_1", "web_designer", "unmapped_profession_2"],
-            "job_stability": ["variable", "moderate", "variable", "moderate"]  # .fillna("moderate")
-        })  
-        # Ensure actual and expected output DataFrames are identical
-        assert_frame_equal(X_transformed, expected_X_transformed)
-
-    # Ensure .transform() assigns "moderate" job stability for missing values in profession column
-    @pytest.mark.unit
-    @pytest.mark.parametrize("missing_value", [None, np.nan])
-    def test_transform_assigns_moderate_to_missing_professions(self, transformer, missing_value):
-        X_with_missing_value = pd.DataFrame({
-            "profession": ["artist", missing_value, "web_designer"]
-        })        
-        # Fit and transform
-        transformer.fit(X_with_missing_value)
-        X_transformed = transformer.transform(X_with_missing_value)
-        # Create expected output
-        expected_X_transformed = pd.DataFrame({
-            "profession": ["artist", missing_value, "web_designer"],
-            "job_stability": ["variable", "moderate", "variable"]  # .fillna("moderate")
-        }) 
-        # Ensure actual and expected output DataFrames are identical
-        assert_frame_equal(X_transformed, expected_X_transformed) 
-
     # Ensure .transform() ignores other columns 
     @pytest.mark.unit
     def test_transform_ignores_other_columns(self, transformer, X_input):
@@ -268,27 +233,3 @@ class TestJobStabilityTransformer(BaseTransformerTests):
         X_transformed_without_profession = X_transformed[other_columns]
         # Ensure untransformed and transformed DataFrames of other columns are identical
         assert_frame_equal(X_without_profession, X_transformed_without_profession)
-
-    # Ensure .transform() raises TypeError for "profession" values with invalid data types (must be strings or missing values)
-    @pytest.mark.unit
-    @pytest.mark.parametrize("invalid_profession_data_type", [
-        ["a", "list"],
-        ("a", "tuple"),
-        {"a": "dictionary"},
-        {"a", "set"}, 
-        1,
-        1.23,
-        False
-    ])
-    def test_transform_raises_type_error_for_invalid_profession(self, transformer, invalid_profession_data_type):
-        X = pd.DataFrame({
-            "profession": ["artist", "computer_hardware_engineer", "web_designer"]
-        })
-        transformer.fit(X)
-        X_with_invalid_profession = pd.DataFrame({
-            "profession": ["artist", invalid_profession_data_type, "web_designer"]
-        })
-        expected_error_message = "All values in 'profession' column must be strings or missing values."
-        with pytest.raises(TypeError, match=expected_error_message):
-            transformer.transform(X_with_invalid_profession)  
-    
