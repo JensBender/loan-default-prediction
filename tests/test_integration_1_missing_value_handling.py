630f4b903a8cb98f0a3a84b1fcf3557a1a7c198f
Jens Bender <jens.bender.001@gmail.com>
2025-07-21 15:13:12 +0200

    In tests/test_integration_1_missing_value_handling.py: Add test function test_pipeline_fit_raises_missing_value_error_for_critical_features() and make pipeline segment a fixture

--- tests/test_integration_1_missing_value_handling.py
+++ tests/test_integration_1_missing_value_handling.py
@@ -39,11 +39,33 @@ def X_input():
 
 
 # --- Pipeline Segment to Be Integration Tested ---
-pipeline_segment = Pipeline([
-    ("missing_value_checker", MissingValueChecker(critical_features=CRITICAL_FEATURES, non_critical_features=NON_CRITICAL_FEATURES)),
-    ("missing_value_handler", ColumnTransformer(
-        transformers=[("categorical_imputer", SimpleImputer(strategy="most_frequent").set_output(transform="pandas"), NON_CRITICAL_FEATURES)],
-        remainder="passthrough",
-        verbose_feature_names_out=False  # preserve input column names instead of adding prefix 
-    ).set_output(transform="pandas")),  # output pd.DataFrame instead of np.array 
-])
+@pytest.fixture
+def pipeline_segment(): 
+    return Pipeline([
+        ("missing_value_checker", MissingValueChecker(critical_features=CRITICAL_FEATURES, non_critical_features=NON_CRITICAL_FEATURES)),
+        ("missing_value_handler", ColumnTransformer(
+            transformers=[("categorical_imputer", SimpleImputer(strategy="most_frequent").set_output(transform="pandas"), NON_CRITICAL_FEATURES)],
+            remainder="passthrough",
+            verbose_feature_names_out=False  # preserve input column names instead of adding prefix 
+        ).set_output(transform="pandas")),  # output pd.DataFrame instead of np.array 
+    ])
+
+
+# --- Test Cases ---
+@pytest.mark.integration
+@pytest.mark.parametrize("missing_value", [None, np.nan])
+@pytest.mark.parametrize("critical_feature", CRITICAL_FEATURES)
+def test_pipeline_fit_raises_missing_value_error_for_critical_features(X_input, pipeline_segment, missing_value, critical_feature):
+        X_with_missing_values = X_input.copy()
+        X_with_missing_values[critical_feature] = missing_value
+        # Create expected dictionary of number of missing values by column 
+        expected_missing_by_column_dict = {"income": 0, "age": 0, "experience": 0, "profession": 0, "city": 0, "state": 0, "current_job_yrs": 0, "current_house_yrs": 0}
+        expected_missing_by_column_dict[critical_feature] = 6  # X_input has 6 rows
+        # Create expected error message text
+        expected_error_message = (
+            f"6 missing values found in critical features "
+            f"across 6 rows. Please provide missing values.\n"
+            f"Missing values by column: {expected_missing_by_column_dict}" 
+        )
+        with pytest.raises(MissingValueError, match=expected_error_message):
+            pipeline_segment.fit(X_with_missing_values)
