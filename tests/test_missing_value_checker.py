370d4fc659984a92f414d16ec71054832831f3bb
Jens Bender <jens.bender.001@gmail.com>
2025-07-21 09:33:36 +0200

    In tests/test_missing_value_checker.py: Add method .test_fit_raises_missing_value_error_for_critical_features()

--- tests/test_missing_value_checker.py
+++ tests/test_missing_value_checker.py
@@ -147,6 +147,25 @@ class TestMissingValueChecker(BaseTransformerTests):
         with pytest.raises(ColumnMismatchError):
             transformer.fit(X_with_unexpected_columns)
 
+    # Ensure .fit() raises MissingValueError for missing values in critical features
+    @pytest.mark.unit
+    @pytest.mark.parametrize("missing_value", [None, np.nan])
+    @pytest.mark.parametrize("critical_feature", CRITICAL_FEATURES)
+    def test_fit_raises_missing_value_error_for_critical_features(self, transformer, X_input, missing_value, critical_feature):
+        X_with_missing_value = X_input.copy()
+        X_with_missing_value[critical_feature] = missing_value
+        # Create expected dictionary of number of missing values by column 
+        expected_missing_by_column_dict = {"income": 0, "age": 0, "experience": 0, "profession": 0, "city": 0, "state": 0, "current_job_yrs": 0, "current_house_yrs": 0}
+        expected_missing_by_column_dict[critical_feature] = 6  # X_input has 6 rows
+        # Create expected error message text
+        expected_error_message = (
+            f"6 missing values found in critical features "
+            f"across 6 rows. Please provide missing values.\n"
+            f"Missing values by column: {expected_missing_by_column_dict}" 
+        )
+        with pytest.raises(MissingValueError, match=expected_error_message):
+            transformer.fit(X_with_missing_value)
+
     # Ensure .transform() raises ColumnMismatchError for missing columns in the input DataFrame
     @pytest.mark.unit
     @pytest.mark.parametrize("missing_columns", [
