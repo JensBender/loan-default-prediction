c8b38adcc682fd5c3bc49342bea78a6dec33bcb3
Jens Bender <jens.bender.001@gmail.com>
2025-07-22 12:45:52 +0200

    In tests/test_integration_1_missing_value_handling.py: Ensure fit learns mode of non-critical features in .test_pipeline_fit_warns_and_learns_mode_for_missing_values_in_non_critical_features()

--- tests/test_integration_1_missing_value_handling.py
+++ tests/test_integration_1_missing_value_handling.py
@@ -85,14 +85,19 @@ def test_pipeline_fit_and_transform_raise_missing_value_error_for_critical_featu
 @pytest.mark.parametrize("missing_value", [None, np.nan])
 @pytest.mark.parametrize("non_critical_feature", NON_CRITICAL_FEATURES)
 def test_pipeline_fit_warns_and_learns_mode_for_missing_values_in_non_critical_features(X_input, pipeline, missing_value, non_critical_feature, capsys):
-        X_with_missing_values = X_input.copy()
-        X_with_missing_values.loc[0, non_critical_feature] = missing_value  # use first row as a representative example
+        X_with_missing_value = X_input.copy()
+        X_with_missing_value.loc[0, non_critical_feature] = missing_value  # use first row as a representative example
         # Ensure .fit() prints warning message
-        pipeline.fit(X_with_missing_values)
+        pipeline.fit(X_with_missing_value)
         captured_output_and_error = capsys.readouterr()
         warning_message = captured_output_and_error.out
         assert "Warning" in warning_message 
         assert "1 missing value found in non-critical features" in warning_message
         assert "will be imputed" in warning_message
         assert captured_output_and_error.err == ""
-        # Ensure .fit() learns mode (most frequent value) of non-critical feature
+        # Ensure .fit() learns mode (most frequent value) of each non-critical feature
+        X_transformed = pipeline.transform(X_with_missing_value)
+        assert X_transformed.loc[0, "married"] == "single"
+        assert X_transformed.loc[0, "house_ownership"] == "rented"
+        assert X_transformed.loc[0, "car_ownership"] == "no"
+        
