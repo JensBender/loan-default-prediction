bccdf5e2fa32728cab2522de15e905bc1e33cdce
Jens Bender <jens.bender.001@gmail.com>
2025-07-24 09:17:12 +0200

    In tests/base_pipeline_tests.py: Add BasePipelineTests class method .test_pipeline_transform_raises_value_error_for_wrong_column_order()

--- tests/base_pipeline_tests.py
+++ tests/base_pipeline_tests.py
@@ -1,5 +1,4 @@
 import pytest
-from sklearn.pipeline import Pipeline
 from sklearn.base import clone
 from sklearn.exceptions import NotFittedError
 import pandas as pd
@@ -10,4 +9,16 @@ import pickle
 
 # Base tests for a sklearn pipeline or subsegment of a pipeline (that individual integration test classes can inherit from)
 class BasePipelineTests:
-    pass
+    # Ensure pipeline .transform() raises ValueError if columns are in different order than during .fit()
+    @pytest.mark.integration
+    def test_pipeline_transform_raises_value_error_for_wrong_column_order(X_input, pipeline):
+        X = X_input.copy()
+        # Fit on original DataFrame X
+        pipeline.fit(X)
+        # Create DataFrame with different column order than during .fit()
+        reversed_columns = X.columns[::-1]  # reverse order as an example
+        X_with_wrong_column_order = X[reversed_columns]  
+        # Ensure .transform() on wrong column order raises ValueError
+        expected_error_message = "Feature names and feature order of input X must be the same as during .fit()."
+        with pytest.raises(ValueError, match=expected_error_message):
+            pipeline.transform(X_with_wrong_column_order)
