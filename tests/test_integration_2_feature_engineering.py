3b0e58f7fb5296a88c69e9a5c9269785573717a3
Jens Bender <jens.bender.001@gmail.com>
2025-07-25 14:08:57 +0200

    In tests/test_integration_2_feature_engineering.py: Update method to .test_feature_engineering_pipeline_fit_and_transform_raise_categorical_label_error_for_unknown_labels() and parametrize for method' fit and transform

--- tests/test_integration_2_feature_engineering.py
+++ tests/test_integration_2_feature_engineering.py
@@ -133,12 +133,19 @@ class TestFeatureEngineeringPipeline(BaseSupervisedPipelineTests):
 
     # Ensure pipeline .transform() raises CategoricalLabelError for unknown labels 
     @pytest.mark.integration
+    @pytest.mark.parametrize("method", ["fit", "transform"])
     @pytest.mark.parametrize("column", ["married", "car_ownership"])
-    def test_feature_engineering_pipeline_transform_raises_categorical_label_error_for_unknown_labels(self, X_input, y_input, pipeline, column):
+    def test_feature_engineering_pipeline_fit_and_transform_raise_categorical_label_error_for_unknown_labels(self, X_input, y_input, pipeline, method, column):
         X = X_input.copy()
         y = y_input.copy()
         X_with_unknown_label = X_input.copy()
         X_with_unknown_label.loc[0, column] = "unknown_label"  # modify first row as a representative example
-        pipeline.fit(X, y)
-        with pytest.raises(CategoricalLabelError):
-            pipeline.transform(X_with_unknown_label)
+        # Ensure .fit() raises CategoricalLabelError 
+        if method == "fit":
+            with pytest.raises(CategoricalLabelError):
+                pipeline.fit(X_with_unknown_label, y)
+        # Ensure .transform() raises CategoricalLabelError 
+        else:
+            pipeline.fit(X, y) 
+            with pytest.raises(CategoricalLabelError):
+                pipeline.transform(X_with_unknown_label)
