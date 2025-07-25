f9c657ef092e69cfb5374884a59ba40ecb15255b
Jens Bender <jens.bender.001@gmail.com>
2025-07-25 08:23:24 +0200

    In tests/test_integration_2_feature_engineering.py: Add method .test_feature_engineering_pipeline_happy_path()

--- tests/test_integration_2_feature_engineering.py
+++ tests/test_integration_2_feature_engineering.py
@@ -78,4 +78,29 @@ def pipeline():
 # BaseSupervisedPipelineTests further inherits the following test from BasePipelineTests:
 # .test_pipeline_transform_raises_not_fitted_error_if_unfitted()
 class TestFeatureEngineeringPipeline(BaseSupervisedPipelineTests):
-    pass
+    # Ensure pipeline correctly engineers features
+    @pytest.mark.integration
+    def test_feature_engineering_pipeline_happy_path(self, pipeline, X_input, y_input):
+        X = X_input.copy()
+        y = y_input.copy()
+        pipeline.fit(X, y)
+        X_transformed = pipeline.transform(X)
+        expected_X_transformed = pd.DataFrame({
+            "income": [9121364, 2636544, 9470213, 6558967, 6245331, 154867],
+            "age": [70, 39, 41, 41, 65, 64],
+            "experience": [18, 0, 5, 10, 6, 1],
+            "married": [False, False, False, True, False, False],
+            "house_ownership": ["rented", "rented", "norent_noown", "rented", "rented", "owned"],
+            "car_ownership": [False, False, True, False, False, False],
+            "profession": ["artist", "computer_hardware_engineer", "web_designer", "comedian", 
+                           "financial_analyst", "statistician"],
+            "city": ["sikar", "vellore", "bidar", "bongaigaon", "eluru[25]", "danapur"],
+            "state": ["rajasthan", "tamil_nadu", "karnataka", "assam", "andhra_pradesh", "bihar"],
+            "current_job_yrs": [3, 0, 5, 10, 6, 1],
+            "current_house_yrs": [11, 11, 13, 12, 12, 12],
+            "job_stability": ["variable", "moderate", "variable", "variable", "moderate", "moderate"],
+            "city_tier": ["unknown", "unknown", "unknown", "unknown", "unknown", "tier_3"],
+            "state_default_rate": [0.0, 1.0, 0.0, 0.0, 1.0, 0.0],
+        })
+        # Ensure actual and expected output DataFrames are identical
+        assert_frame_equal(X_transformed, expected_X_transformed)
