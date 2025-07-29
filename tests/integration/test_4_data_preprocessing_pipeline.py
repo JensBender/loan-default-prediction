b0d73088c40bbbd4962059ae55bb0d8b49b3661b
Jens Bender <jens.bender.001@gmail.com>
2025-07-29 15:02:14 +0200

    In tests/integration/test_4_data_preprocessing_pipeline.py: Add X_input fixture

--- tests/integration/test_4_data_preprocessing_pipeline.py
+++ tests/integration/test_4_data_preprocessing_pipeline.py
@@ -41,3 +41,23 @@ from app.global_constants import (
     COLUMNS_TO_KEEP
 )
 from tests.integration.base_pipeline_tests import BaseSupervisedPipelineTests
+
+
+# --- Fixtures ---
+# Fixture to create X input DataFrame for use in tests
+@pytest.fixture
+def X_input():
+    return pd.DataFrame({
+        "income": [9121364, 2636544, 9470213, 6558967, 6245331, 154867],
+        "age": [70, 39, 41, 41, 65, 64],
+        "experience": [18, 0, 5, 10, 6, 1],
+        "married": ["single", "single", "single", "married", "single", "single"],
+        "house_ownership": ["rented", "rented", "norent_noown", "rented", "rented", "owned"],
+        "car_ownership": ["no", "no", "yes", "no", "no", "no"],
+        "profession": ["Artist", "Computer_hardware_engineer", "Web_designer", "Comedian", 
+                       "Financial_Analyst", "Statistician"],
+        "city": ["Sikar", "Vellore", "Bidar", "Bongaigaon", "Eluru[25]", "Danapur"],
+        "state": ["Rajasthan", "Tamil_Nadu", "Karnataka", "Assam", "Andhra_Pradesh", "Bihar"],
+        "current_job_yrs": [3, 0, 5, 10, 6, 1],
+        "current_house_yrs": [11, 11, 13, 12, 12, 12],
+    })
