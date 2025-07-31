ced4a909608b45c14e896ba046db8c77ec4b89ab
Jens Bender <jens.bender.001@gmail.com>
2025-07-31 11:16:00 +0200

    In tests/unit/test_robust_standard_scaler.py: Add method .test_transform_handles_empty_df()

--- tests/unit/test_robust_standard_scaler.py
+++ tests/unit/test_robust_standard_scaler.py
@@ -6,6 +6,8 @@ import sys
 import pytest
 import pandas as pd
 from pandas.testing import assert_frame_equal
+import numpy as np
+from numpy.testing import assert_array_equal
 from sklearn.preprocessing import StandardScaler
 
 # Add the root directory to the path for local imports (by going up two levels from current directory in which this file lives)
@@ -41,3 +43,17 @@ class TestRobustStandardScaler:
     def test_instantiation(self, transformer):
         assert isinstance(transformer, StandardScaler)
         assert isinstance(transformer, RobustStandardScaler)
+
+    # Ensure .transform() correctly handles an empty DataFrame
+    @pytest.mark.unit
+    def test_transform_handles_empty_df(self, transformer, X_input):
+        X = X_input.copy()
+        X_empty = pd.DataFrame(columns=X.columns)
+        # Fit on non-empty DataFrame, but transform on empty DataFrame
+        transformer.fit(X)
+        X_transformed_empty = transformer.transform(X_empty)
+        # Create expected output (empty numpy array with 0 rows and column equal to input DataFrame)
+        n_columns = X.shape[1]
+        expected_X_transformed_empty = np.empty((0, n_columns))
+        # Ensure output is as expected
+        assert_array_equal(X_transformed_empty, expected_X_transformed_empty)
