3ee34aaa11231cf97c996938442931faf250e1fd
Jens Bender <jens.bender.001@gmail.com>
2025-08-01 10:56:46 +0200

    In tests/integration/: Add test_5_data_preprocessing_and_model_pipeline.py with imports

---
+++ tests/integration/test_5_data_preprocessing_and_model_pipeline.py
@@ -0,0 +1,49 @@
+# Standard library imports
+import os
+import sys
+
+# Third-party library imports
+import pytest
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+
+# Add the root directory to the path for local imports (by going up two levels from current directory in which this file lives)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+# Local imports
+from app.custom_transformers import (
+    MissingValueChecker, 
+    MissingValueStandardizer, 
+    RobustSimpleImputer,
+    SnakeCaseFormatter, 
+    BooleanColumnTransformer, 
+    JobStabilityTransformer, 
+    CityTierTransformer, 
+    StateDefaultRateTargetEncoder,
+    RobustStandardScaler,
+    RobustOneHotEncoder,
+    RobustOrdinalEncoder,
+    FeatureSelector,
+    MissingValueError, 
+    ColumnMismatchError,
+    CategoricalLabelError
+)
+from app.global_constants import (
+    CRITICAL_FEATURES, 
+    NON_CRITICAL_FEATURES, 
+    COLUMNS_FOR_SNAKE_CASING,
+    BOOLEAN_COLUMN_MAPPINGS,
+    JOB_STABILITY_MAP,
+    CITY_TIER_MAP,
+    NUMERICAL_COLUMNS, 
+    NOMINAL_COLUMN_CATEGORIES, 
+    ORDINAL_COLUMN_ORDERS, 
+    COLUMNS_TO_KEEP,
+    RF_BEST_PARAMS
+)
+from tests.integration.base_pipeline_tests import BaseSupervisedPipelineTests
+
+
