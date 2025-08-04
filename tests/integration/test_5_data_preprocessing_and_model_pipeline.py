047f1e99fe966670dc5e96e89e52cceacd113d08
Jens Bender <jens.bender.001@gmail.com>
2025-08-04 12:03:56 +0200

    In tests/integration/test_5_data_preprocessing_and_model_pipeline.py: Ensure .fit() print warning message in .test_data_preprocessing_and_model_pipeline_imputes_missing_values_in_non_critical_features()

--- tests/integration/test_5_data_preprocessing_and_model_pipeline.py
+++ tests/integration/test_5_data_preprocessing_and_model_pipeline.py
@@ -240,10 +240,18 @@ def test_data_preprocessing_and_model_pipeline_raises_missing_value_error_for_cr
             with pytest.raises(MissingValueError):
                 pipeline.predict_proba(X_with_missing_value)
 
-# Ensure pipeline .fit() warns about missing values in non-critical features and .predict() and .predict_proba() impute the mode
+# Ensure pipeline .predict() and .predict_proba() impute the mode for missing values in non-critical features
 @pytest.mark.integration
 @pytest.mark.parametrize("method", ["fit", "predict", "predict_proba"])
 @pytest.mark.parametrize("missing_value", [None, np.nan, pd.NA])
 @pytest.mark.parametrize("non_critical_feature", NON_CRITICAL_FEATURES)
-def test_data_preprocessing_and_model_pipeline_imputes_missing_values_in_non_critical_features(X_input, y_input, pipeline, method, missing_value, non_critical_feature):
-    pass  # to do
+def test_data_preprocessing_and_model_pipeline_imputes_missing_values_in_non_critical_features(X_input, y_input, pipeline, method, missing_value, non_critical_feature, capsys):
+    X = X_input.copy()
+    y = y_input.copy()
+    X_with_missing_value = X_input.copy()
+    X_with_missing_value.loc[0, non_critical_feature] = missing_value
+    # Ensure .fit() on missing value prints warning message
+    if method == "fit":
+        pipeline.fit(X_with_missing_value, y)
+        # categorical_imputer = pipeline.named_steps
+        assert "Warning" in capsys.readouterr().out
