af50f4d9d7291d2bd0dd09a175a9494a7909d379
Jens Bender <jens.bender.001@gmail.com>
2025-08-13 12:22:59 +0200

    In api/app.py: Use Pydantic StrictInt and StrictFloat for all numerical inputs

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ import pickle
 # Third-party library imports
 import pandas as pd
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, StrictInt, StrictFloat
 import uvicorn
 
 # Create FastAPI app
@@ -15,17 +15,17 @@ app = FastAPI()
 
 # Pydantic data model: Pipeline input dictionary
 class pipeline_input_dict(BaseModel):
-    age: int | float
+    age: StrictInt | StrictFloat
     married: str
-    income: int | float
+    income: StrictInt | StrictFloat
     car_ownership: str
     house_ownership: str 
-    current_house_yrs: int | float
+    current_house_yrs: StrictInt | StrictFloat
     city: str 
     state: str 
     profession: str 
-    experience: int | float
-    current_job_yrs: int | float
+    experience: StrictInt | StrictFloat
+    current_job_yrs: StrictInt | StrictFloat
 
 
 # Launch API
