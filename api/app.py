83eb019e7a7f6b98075f71def95bd302997a48b6
Jens Bender <jens.bender.001@gmail.com>
2025-08-13 12:13:07 +0200

    In api/app.py: Add Pydantic data model for pipeline input dictionary

--- api/app.py
+++ api/app.py
@@ -6,10 +6,27 @@ import pickle
 # Third-party library imports
 import pandas as pd
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
 # Create FastAPI app
 app = FastAPI()
+
+
+# Pydantic data model: Pipeline input dictionary
+class pipeline_input_dict(BaseModel):
+    age: int | float
+    married: str
+    income: int | float
+    car_ownership: str
+    house_ownership: str 
+    current_house_yrs: int | float
+    city: str 
+    state: str 
+    profession: str 
+    experience: int | float
+    current_job_yrs: int | float
+
 
 # Launch API
 if __name__ == "__main__":
