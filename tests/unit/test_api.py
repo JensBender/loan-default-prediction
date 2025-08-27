2f5ea64f17a6afc7b3f87fa652f7421f1056a2a8
Jens Bender <jens.bender.001@gmail.com>
2025-08-27 15:02:17 +0200

    In tests/unit/test_api.py: Add function .test_pipeline_input_happy_path()

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -38,3 +38,23 @@ def valid_pipeline_input():
         "experience": 10,
         "current_job_yrs": 7        
     }
+
+
+# --- Pydantic Model Classes ---
+@pytest.mark.unit
+def test_pipeline_input_happy_path(valid_pipeline_input):
+    pipeline_input = PipelineInput(**valid_pipeline_input)
+    assert pipeline_input == PipelineInput(
+        age=30,
+        married="married",
+        income=1000000,
+        car_ownership="yes",
+        house_ownership="rented",
+        current_house_yrs=12,
+        city="delhi_city",
+        state="assam",
+        profession="architect",
+        experience=10,
+        current_job_yrs=7          
+    )
+    
