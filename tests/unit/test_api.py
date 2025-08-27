006470c73cf6f547a84b2b4cb6bfbfb4263219a3
Jens Bender <jens.bender.001@gmail.com>
2025-08-27 15:14:58 +0200

    In tests/unit/test_api.py > .test_pipeline_input_happy_path(): Assert each field individually instead of the a single assert for entire PipelineInput instance

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -45,16 +45,14 @@ def valid_pipeline_input():
 @pytest.mark.unit
 def test_pipeline_input_happy_path(valid_pipeline_input: Dict[str, Any]) -> None:
     pipeline_input = PipelineInput(**valid_pipeline_input)
-    assert pipeline_input == PipelineInput(
-        age=30,
-        married="married",
-        income=1000000,
-        car_ownership="yes",
-        house_ownership="rented",
-        current_house_yrs=12,
-        city="delhi_city",
-        state="assam",
-        profession="architect",
-        experience=10,
-        current_job_yrs=7          
-    )
+    assert pipeline_input.age == 30
+    assert pipeline_input.married == "married"
+    assert pipeline_input.income == 1000000
+    assert pipeline_input.car_ownership == "yes"
+    assert pipeline_input.house_ownership == "rented"
+    assert pipeline_input.current_house_yrs == 12
+    assert pipeline_input.city == "delhi_city"
+    assert pipeline_input.state == "assam"
+    assert pipeline_input.profession == "architect"
+    assert pipeline_input.experience == 10
+    assert pipeline_input.current_job_yrs == 7
