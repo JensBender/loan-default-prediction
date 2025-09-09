198d3a2ce096b4ce949d77fefbd92f31cf18e413
Jens Bender <jens.bender.001@gmail.com>
2025-09-09 14:08:06 +0200

    In tests/unit/test_api.py > TestLoadPipeline class: Add assertion for error message in method .test_raises_file_not_found_error_for_non_existent_file()

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -9,5 +9,8 @@ from api.app import load_pipeline
 # --- Function .load_pipeline() ---
 class TestLoadPipeline:
     def test_raises_file_not_found_error_for_non_existent_file(self):
-        with pytest.raises(FileNotFoundError):
+        with pytest.raises(FileNotFoundError) as exc_info:
             load_pipeline(path="non_existent_file.joblib")
+        error_msg = str(exc_info.value)
+        assert "Pipeline file not found at" in error_msg
+        assert "non_existent_file.joblib" in error_msg 
