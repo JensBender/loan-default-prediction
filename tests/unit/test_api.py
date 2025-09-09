716104564d3cbb6df0416c99b8413c28607016de
Jens Bender <jens.bender.001@gmail.com>
2025-09-09 13:56:34 +0200

    In tests/unit/test_api.py > TestLoadPipeline class: Add method .test_raises_file_not_found_error_for_non_existent_file()

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -8,4 +8,6 @@ from api.app import load_pipeline
 
 # --- Function .load_pipeline() ---
 class TestLoadPipeline:
-    pass
+    def test_raises_file_not_found_error_for_non_existent_file(self):
+        with pytest.raises(FileNotFoundError):
+            load_pipeline(path="non_existent_file.joblib")
