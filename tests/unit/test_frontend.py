030f5a44eb60bc4d98a51f1825d0d6e2e5f74089
Jens Bender <jens.bender.001@gmail.com>
2025-09-26 10:21:16 +0200

    In tests/unit/test_frontend.py: Add class TestFormatSnakeCase rename method to .test_happy_path() and add method .test_non_string_values_remain_unchanged()

--- tests/unit/test_frontend.py
+++ tests/unit/test_frontend.py
@@ -17,37 +17,52 @@ from frontend.app import (
 )
 
 
-# --- .format_snake_case() ---
-# Happy path
-@pytest.mark.unit
-@pytest.mark.parametrize("input, expected_output", [
-    ("   leading spaces", "leading_spaces"),
-    ("trailing spaces   ", "trailing_spaces"),
-    ("  Leading and Trailing Spaces  ", "leading_and_trailing_spaces"),
-    ("\tLeading tab and trailing linebreak\n", "leading_tab_and_trailing_linebreak"),
-    ("\n\t", ""),
-    ("Multiple  Inner   Spaces", "multiple_inner_spaces"),
-    ("Title Case", "title_case"),
-    ("MiXeD CaSe", "mixed_case"),
-    ("", ""),
-    ("   ", ""),
-    ("innner\ttab", "innner_tab"),
-    ("inner\nnewline", "inner_newline"),
-    ("with-mixed_chars 123", "with_mixed_chars_123"),
-    ("singleword", "singleword"),
-    (123, 123),
-    (123.45, 123.45),
-    (True, True),
-    (None, None),
-    (["a", "list"], ["a", "list"]),
-    (("a", "tuple"), ("a", "tuple")),
-    ({"a": "dictionary"}, {"a": "dictionary"}),
-])
-def test_format_snake_case_happy_path(input, expected_output):
-    assert format_snake_case(input) == expected_output
+# --- Function .format_snake_case() ---
+class TestFormatSnakeCase:
+    # Happy path
+    @pytest.mark.unit
+    @pytest.mark.parametrize("input, expected_output", [
+        ("   leading spaces", "leading_spaces"),
+        ("trailing spaces   ", "trailing_spaces"),
+        ("  Leading and Trailing Spaces  ", "leading_and_trailing_spaces"),
+        ("\tLeading tab and trailing linebreak\n", "leading_tab_and_trailing_linebreak"),
+        ("\n\t", ""),
+        ("Multiple  Inner   Spaces", "multiple_inner_spaces"),
+        ("Title Case", "title_case"),
+        ("MiXeD CaSe", "mixed_case"),
+        ("", ""),
+        ("   ", ""),
+        ("innner\ttab", "innner_tab"),
+        ("inner\nnewline", "inner_newline"),
+        ("with-mixed_chars 123", "with_mixed_chars_123"),
+        ("singleword", "singleword"),
+        (123, 123),
+        (123.45, 123.45),
+        (True, True),
+        (None, None),
+        (["a", "list"], ["a", "list"]),
+        (("a", "tuple"), ("a", "tuple")),
+        ({"a": "dictionary"}, {"a": "dictionary"}),
+    ])
+    def test_happy_path(self, input, expected_output):
+        assert format_snake_case(input) == expected_output
+    
+    # Non-string values remain unchanged
+    @pytest.mark.unit
+    @pytest.mark.parametrize("non_string_value", [
+        1,
+        1.23,
+        False,
+        None,  
+        ["a", "list"],
+        ("a", "tuple"),
+        {"a": "dictionary"},
+        {"a", "set"}        
+    ])
+    def test_non_string_values_remain_unchanged(self, non_string_value):
+        assert format_snake_case(non_string_value) == non_string_value
 
-
-# --- .snake_case_str_values_in_dict() ---
+# --- Function .snake_case_str_values_in_dict() ---
 # Happy path
 @pytest.mark.unit
 def test_snake_case_str_values_in_dict_happy_path():
@@ -98,7 +113,7 @@ def test_snake_case_formatted_inputs_remain_unchanged():
     assert snake_case_str_values_in_dict(inputs_with_snake_case) == inputs_with_snake_case
 
 
-# --- .format_house_ownership() ---
+# --- Function .format_house_ownership() ---
 # Happy path
 @pytest.mark.parametrize("display_label, expected_pipeline_label", [
     ("neither_rented_nor_owned", "norent_noown"),
@@ -108,5 +123,5 @@ def test_snake_case_formatted_inputs_remain_unchanged():
 def test_format_house_ownership_happy_path(display_label, expected_pipeline_label):
     assert format_house_ownership(display_label) == expected_pipeline_label
 
-# --- ._format_validation_error() ---
-# --- .predict_loan_default() ---
+# --- Function ._format_validation_error() ---
+# --- Function .predict_loan_default() ---
