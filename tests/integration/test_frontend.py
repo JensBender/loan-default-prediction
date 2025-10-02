ce7dc9acaf5143dd1b544b1ee5a9bbff3bd32589
Jens Bender <jens.bender.001@gmail.com>
2025-10-02 11:13:10 +0200

    In tests/integration/test_frontend.py > TestPredictLoanDefault: Add method .test_happy_path()

--- tests/integration/test_frontend.py
+++ tests/integration/test_frontend.py
@@ -12,4 +12,44 @@ from backend.app import app
 
 # --- Function .predict_loan_default() ---
 class TestPredictLoanDefault:
-    pass
+    # Happy path
+    @pytest.mark.integration
+    @patch("frontend.app.requests.post")
+    def test_happy_path(self, mock_post_request):
+        # Raw inputs from Gradio UI as dictionary
+        inputs = {
+            "age": 30, 
+            "married": "Single", 
+            "income": 300000, 
+            "car_ownership": "No", 
+            "house_ownership": "Neither Rented Nor Owned", 
+            "current_house_yrs": 11, 
+            "city": "Sikar", 
+            "state": "Rajasthan", 
+            "profession": "Artist", 
+            "experience": 3, 
+            "current_job_yrs": 3
+        }
+        # Mock the post request to redirect it to the FastAPI test client
+        client = TestClient(app)
+        response = client.post("/predict", json=inputs)
+        mock_post_request.return_value = response
+
+        # Call .predict_loan_default()
+        prediction, probabilities = predict_loan_default(**inputs)
+
+        # Ensure requests.post() was called once
+        mock_post_request.assert_called_once()
+        # Ensure post request was successful
+        assert response.status_code == 200
+        # Ensure prediction is as expected
+        assert isinstance(prediction, str)
+        assert prediction in ["Default", "No Default"]
+        # Ensure probabilities is as expected
+        assert isinstance(probabilities, dict)
+        assert "Defaut" in probabilities
+        assert "No Default" in probabilities
+        assert isinstance(probabilities["Default"], float)
+        assert isinstance(probabilities["No Default"], float)
+        # Ensure probabilities sum to approximately 1
+        assert (probabilities["Default"] + probabilities["No Default"]) == pytest.approx(1.0)  # default relative tolerance of 1e-6 (0.0001%) and absolute tolerance of 1e-12
