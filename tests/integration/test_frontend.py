0ada2a02907d640663497b72f39fa06c1900013c
Jens Bender <jens.bender.001@gmail.com>
2025-10-02 11:28:50 +0200

    In tests/integration/test_frontend.py > TestPredictLoanDefault: Use mock_post_request side effect to redirect request to testclient in method .test_happy_path()

--- tests/integration/test_frontend.py
+++ tests/integration/test_frontend.py
@@ -1,10 +1,14 @@
 # --- Imports ---
 # Standard library imports
+import warnings
 from unittest.mock import patch
 
 # Third-party library imports
 import pytest
 from fastapi.testclient import TestClient
+
+# Suppress deprecation warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # Local imports
 from frontend.app import predict_loan_default
@@ -31,23 +35,22 @@ class TestPredictLoanDefault:
             "current_job_yrs": 3
         }
         # Mock the post request to redirect it to the FastAPI test client
-        client = TestClient(app)
-        response = client.post("/predict", json=inputs)
-        mock_post_request.return_value = response
+        client = TestClient(app)        
+        def redirect_post_request_to_testclient(url, json, timeout):
+            return client.post("/predict", json=json)
+        mock_post_request.side_effect = redirect_post_request_to_testclient
 
         # Call .predict_loan_default()
         prediction, probabilities = predict_loan_default(**inputs)
 
         # Ensure requests.post() was called once
         mock_post_request.assert_called_once()
-        # Ensure post request was successful
-        assert response.status_code == 200
         # Ensure prediction is as expected
         assert isinstance(prediction, str)
         assert prediction in ["Default", "No Default"]
         # Ensure probabilities is as expected
         assert isinstance(probabilities, dict)
-        assert "Defaut" in probabilities
+        assert "Default" in probabilities
         assert "No Default" in probabilities
         assert isinstance(probabilities["Default"], float)
         assert isinstance(probabilities["No Default"], float)
