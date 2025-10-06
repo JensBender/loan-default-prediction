a0bb8803b90ba86d6a82a2122c513c5de7bcae24
Jens Bender <jens.bender.001@gmail.com>
2025-10-06 13:33:29 +0200

    In tests/: Add e2e/test_frontend.py with placeholder for test_user_submits_loan_prediction_form() function marked as e2e test

---
+++ tests/e2e/test_frontend.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+# End-to-end test that simulates a user filling out the form and receiving a prediction in the frontend UI
+@pytest.mark.e2e
+def test_user_submits_loan_prediction_form():
+    pass
