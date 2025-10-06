86b6a6c125305ba2219da30dcd3995b71c5932bf
Jens Bender <jens.bender.001@gmail.com>
2025-10-06 14:12:44 +0200

    In tests/e2e/test_frontend.py: Create webdriver and make get request to frontend Gradio UI

--- tests/e2e/test_frontend.py
+++ tests/e2e/test_frontend.py
@@ -1,7 +1,15 @@
 import pytest
+from selenium import webdriver
 
 
 # End-to-end test that simulates a user filling out the form and receiving a prediction in the frontend UI
 @pytest.mark.e2e
 def test_user_submits_loan_prediction_form():
-    pass
+    # Create Chrome browser
+    driver = webdriver.Chrome()
+
+    # Get request to frontend Gradio UI
+    driver.get("http://localhost:7860")
+
+    # Close Chrome browser window
+    driver.quit()
