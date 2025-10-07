a337e35f7031ba824d1c33d91ef97227f90ec520
Jens Bender <jens.bender.001@gmail.com>
2025-10-07 14:41:57 +0200

    In tests/e2e/test_frontend.py: Identify all 11 input fields

--- tests/e2e/test_frontend.py
+++ tests/e2e/test_frontend.py
@@ -1,3 +1,7 @@
+# Standard library imports
+import time 
+
+# Third-party library imports
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,7 +22,28 @@ def test_user_submits_loan_prediction_form():
         # Enter age
         age_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Age']")))
         age_field.send_keys(30)
+        # Enter married
+        married_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Married/Single']")))
+        # Enter income
+        income_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Income']")))
+        # Enter car_ownership
+        car_ownership_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Car Ownership']")))
+        # Enter house_ownership
+        house_ownership_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='House Ownership']")))
+        # Enter current_house_yrs
+        current_house_yrs_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Current House Years']")))
+        # Enter city
+        city_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='City']")))
+        # Enter state
+        state_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='State']")))
+        # Enter profession
+        profession_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Profession']")))
+        # Enter experience
+        experience_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Experience']")))
+        # Enter current_job_yrs
+        current_job_yrs_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Current Job Years']")))
 
     finally:
+        time.sleep(3)  # remove after dev/test phase
         # Close Chrome browser window
         driver.quit()
