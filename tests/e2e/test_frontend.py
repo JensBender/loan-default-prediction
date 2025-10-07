605af64911944bba0591ce752d1d0924b106b465
Jens Bender <jens.bender.001@gmail.com>
2025-10-07 14:33:26 +0200

    In tests/e2e/test_frontend.py: Identify age field and enter an age

--- tests/e2e/test_frontend.py
+++ tests/e2e/test_frontend.py
@@ -1,5 +1,8 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
 
 
 # End-to-end test that simulates a user filling out the form and receiving a prediction in the frontend UI 
@@ -11,6 +14,10 @@ def test_user_submits_loan_prediction_form():
     try:
         # Get request to frontend Gradio UI running locally (make sure to run the app first)
         driver.get("http://localhost:7860")
+
+        # Enter age
+        age_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Age']")))
+        age_field.send_keys(30)
 
     finally:
         # Close Chrome browser window
