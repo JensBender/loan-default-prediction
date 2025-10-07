931a39e2270675c7681d0655a2fb04dae55e7e74
Jens Bender <jens.bender.001@gmail.com>
2025-10-07 14:13:09 +0200

    In tests/e2e/test_frontend.py: Use try block for all webdriver action and finally block to close webdriver

--- tests/e2e/test_frontend.py
+++ tests/e2e/test_frontend.py
@@ -2,14 +2,16 @@ import pytest
 from selenium import webdriver
 
 
-# End-to-end test that simulates a user filling out the form and receiving a prediction in the frontend UI
+# End-to-end test that simulates a user filling out the form and receiving a prediction in the frontend UI 
 @pytest.mark.e2e
 def test_user_submits_loan_prediction_form():
-    # Create Chrome browser
+    # Create a Chrome webdriver
     driver = webdriver.Chrome()
 
-    # Get request to frontend Gradio UI
-    driver.get("http://localhost:7860")
+    try:
+        # Get request to frontend Gradio UI running locally (make sure to run the app first)
+        driver.get("http://localhost:7860")
 
-    # Close Chrome browser window
-    driver.quit()
+    finally:
+        # Close Chrome browser window
+        driver.quit()
