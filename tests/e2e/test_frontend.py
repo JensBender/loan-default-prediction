d4b76d4188cd1be39c2e3ce98446d20107972411
Jens Bender <jens.bender.001@gmail.com>
2025-10-10 12:58:44 +0200

    In tests/e2e/test_frontend.py: Add function .test_happy_path_with_missing_optional_fields() and rename test with full valid inputs to .test_happy_path()

--- tests/e2e/test_frontend.py
+++ tests/e2e/test_frontend.py
@@ -1,7 +1,4 @@
-# Standard library imports
-import time 
-
-# Third-party library imports
+# Imports
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -61,7 +58,7 @@ def driver() -> WebDriver:
 
 # End-to-end happy path test that simulates a user submitting the form and receiving a prediction in the frontend UI 
 @pytest.mark.e2e
-def test_user_submits_loan_default_prediction_form(driver: WebDriver) -> None:
+def test_happy_path(driver: WebDriver) -> None:
     # Get request to frontend Gradio UI  
     # Make sure the Docker container is running locally and port 7860 is mapped
     driver.get("http://localhost:7860")
@@ -99,6 +96,91 @@ def test_user_submits_loan_default_prediction_form(driver: WebDriver) -> None:
     # Ensure probabilities sum to approximately 100
     sum = default_probability + no_default_probability
     assert 99 <= sum <= 101  # allow for rounding edge cases
+
+
+# End-to-end happy path test that simulates a user submitting the form with missing optional fields and receiving a prediction in the frontend UI 
+@pytest.mark.e2e
+def test_happy_path_with_missing_optional_fields(driver: WebDriver) -> None:
+    # Get request to frontend Gradio UI  
+    # Make sure the Docker container is running locally and port 7860 is mapped
+    driver.get("http://localhost:7860")
+
+    # Set inputs in Gradio UI
+    set_number_input(driver, "Age", 30)
+    # Married/Single input (optional) is missing
+    set_number_input(driver, "Income", 300000)
+    # Car Ownership input (optional) is missing
+    # House Ownership input (optional) is missing
+    set_slider_input(driver, "Current House Years", 11)
+    set_dropdown_input(driver, "City", "Sikar")
+    set_dropdown_input(driver, "State", "Rajasthan")
+    set_dropdown_input(driver, "Profession", "Artist")
+    set_slider_input(driver, "Experience", 3)
+    set_slider_input(driver, "Current Job Years", 3)
+
+    # Click predict button
+    predict_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "predict-button")))
+    predict_button.click()
+
+    # Extract predicted probabilities
+    default_probability_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//div[@id='pred-proba-label']//dt[text()='Default']/following-sibling::dd")))
+    no_default_probability_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//div[@id='pred-proba-label']//dt[text()='No Default']/following-sibling::dd")))
+    default_probability = int(default_probability_element.text.replace("%", ""))
+    no_default_probability = int(no_default_probability_element.text.replace("%", ""))
+    # Extract prediction text 
+    prediction_text = extract_prediction_text(driver)
+    
+    # Ensure prediction text is as expected
+    assert prediction_text in ["Default", "No Default"]
+    # Ensure probabilities are numbers between 0 and 100
+    assert 0 <= default_probability <=100
+    assert 0 <= no_default_probability <= 100
+    # Ensure probabilities sum to approximately 100
+    sum = default_probability + no_default_probability
+    assert 99 <= sum <= 101  # allow for rounding edge cases
+
+
+# End-to-end test that simulates a user submitting the form with missing required fields and receiving an error message in the frontend UI 
+@pytest.mark.e2e
+def test_user_submits_form_with_empty_required_fields(driver: WebDriver) -> None:
+    # Get request to frontend Gradio UI  
+    # Make sure the Docker container is running locally and port 7860 is mapped
+    driver.get("http://localhost:7860")
+
+    # Set inputs in Gradio UI
+    set_number_input(driver, "Age", 30)
+    set_dropdown_input(driver, "Married/Single", "Single")
+    set_number_input(driver, "Income", 300000)
+    set_dropdown_input(driver, "Car Ownership", "No")
+    set_dropdown_input(driver, "House Ownership", "Neither Rented Nor Owned")
+    set_slider_input(driver, "Current House Years", 11)
+    # City input (required) is missing
+    # State input (required) is missing
+    # Profession input (required) is missing
+    set_slider_input(driver, "Experience", 3)
+    set_slider_input(driver, "Current Job Years", 3)
+
+    # Click predict button
+    predict_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "predict-button")))
+    predict_button.click()
+
+    # Extract error message in predicted probabilities element (should be an empty str)
+    # Note: Error path renders str in single h2 element whereas success path renders dict in multiple dl/dd elements
+    probabilities_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//div[@id='pred-proba-label']//h2"))) 
+    error_msg_in_probabilities = probabilities_element.text 
+
+    # Extract error message in prediction text
+    error_msg_in_prediction = extract_prediction_text(driver)
+    
+    # Ensure error message is as expected
+    expected_error_msg = (
+        "Input Error! Please check your inputs and try again.\n"
+        "City: Select a city from the list.\n"
+        "State: Select a state from the list.\n"
+        "Profession: Select a profession from the list.\n"
+    )
+    assert error_msg_in_prediction == expected_error_msg
+    assert error_msg_in_probabilities == ""
 
 
 # End-to-end test that simulates a user submitting out-of-range values and receiving an error message in the frontend UI 
@@ -141,48 +223,3 @@ def test_user_submits_out_of_range_values(driver: WebDriver) -> None:
     )
     assert error_msg_in_prediction == expected_error_msg
     assert error_msg_in_probabilities == ""
-
-
-# End-to-end test that simulates a user submitting a form with missing required fields and receiving an error message in the frontend UI 
-@pytest.mark.e2e
-def test_user_submits_form_with_empty_required_fields(driver: WebDriver) -> None:
-    # Get request to frontend Gradio UI  
-    # Make sure the Docker container is running locally and port 7860 is mapped
-    driver.get("http://localhost:7860")
-
-    # Set inputs in Gradio UI
-    set_number_input(driver, "Age", 30)
-    set_dropdown_input(driver, "Married/Single", "Single")
-    set_number_input(driver, "Income", 300000)
-    set_dropdown_input(driver, "Car Ownership", "No")
-    set_dropdown_input(driver, "House Ownership", "Neither Rented Nor Owned")
-    set_slider_input(driver, "Current House Years", 11)
-    # City input (required) is missing
-    # State input (required) is missing
-    # Profession input (required) is missing
-    set_slider_input(driver, "Experience", 3)
-    set_slider_input(driver, "Current Job Years", 3)
-
-    # Click predict button
-    predict_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "predict-button")))
-    predict_button.click()
-
-    # Extract error message in predicted probabilities element (should be an empty str)
-    # Note: Error path renders str in single h2 element whereas success path renders dict in multiple dl/dd elements
-    probabilities_element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, "//div[@id='pred-proba-label']//h2"))) 
-    error_msg_in_probabilities = probabilities_element.text 
-
-    # Extract error message in prediction text
-    error_msg_in_prediction = extract_prediction_text(driver)
-    
-    # Ensure error message is as expected
-    expected_error_msg = (
-        "Input Error! Please check your inputs and try again.\n"
-        "City: Select a city from the list.\n"
-        "State: Select a state from the list.\n"
-        "Profession: Select a profession from the list.\n"
-    )
-    assert error_msg_in_prediction == expected_error_msg
-    assert error_msg_in_probabilities == ""
-
-    time.sleep(5)  # remove after dev/test phase
