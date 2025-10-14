ecd85ca3b47cccd3a2d5b79d4d4e8312d1c0179f
Jens Bender <jens.bender.001@gmail.com>
2025-10-14 11:25:44 +0200

    Update upload_model_to_huggingface.py

--- upload_model_to_huggingface.py
+++ upload_model_to_huggingface.py
@@ -2,9 +2,20 @@ from huggingface_hub import HfApi
 
 api = HfApi()
 
+# Path to model pipeline on local machine
+pipeline_path = "models/loan_default_rf_pipeline.joblib"
+# Name of Hugging Face Hub repository (make sure to create repo first)
+hf_hub_repo = "JensBender/loan-default-prediction-pipeline"
+# Designated name of model pipeline in the Hugging Face Hub repository
+pipeline_in_repo = "loan_default_rf_pipeline.joblib"
+
+print(f"Uploading {pipeline_path} to {hf_hub_repo}...")
+
 api.upload_file(
-    path_or_fileobj="models/loan_default_rf_pipeline.joblib",
-    path_in_repo="loan_default_rf_pipeline.joblib",
-    repo_id="JensBender/loan-default-prediction-pipeline",
+    path_or_fileobj=pipeline_path,
+    repo_id=hf_hub_repo,
+    path_in_repo=pipeline_in_repo,
     repo_type="model"
 )
+
+print("Upload complete!")
