1390b59017b0c8086343b318e302c3013ebcc6a8
Jens Bender <jens.bender.001@gmail.com>
2025-10-16 09:16:32 +0200

    In frontend/app: Change name of logger

--- frontend/app.py
+++ frontend/app.py
@@ -25,7 +25,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
-logger = logging.getLogger("frontend.app")
+logger = logging.getLogger(__name__)
 
 # --- Constants ---
 # Backend URL to FastAPI predict endpoint
