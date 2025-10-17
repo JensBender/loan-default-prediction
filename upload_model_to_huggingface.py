e1e3259512973c4db6c237453e450d6b25487dde
Jens Bender <jens.bender.001@gmail.com>
2025-10-17 15:30:13 +0200

    In upload_model_to_huggingface.py: Add the pipeline version tag to the latest commit

--- upload_model_to_huggingface.py
+++ upload_model_to_huggingface.py
@@ -8,15 +8,25 @@ pipeline_path = "models/loan_default_rf_pipeline.joblib"
 hf_hub_repo = "JensBender/loan-default-prediction-pipeline"
 # Filename of model pipeline in the Hugging Face Hub repository
 filename = "loan_default_rf_pipeline.joblib"
+# Version tag
+tag = "v1.0"
 
 # Upload pipeline
 print("Uploading pipeline to Hugging Face Hub...")
-
 api.upload_file(
     path_or_fileobj=pipeline_path,
     repo_id=hf_hub_repo,
     path_in_repo=filename,
     repo_type="model"
 )
-
 print(f"Successfully uploaded pipeline '{filename}' to Hugging Face Hub repository '{hf_hub_repo}'.")
+
+# Add the version tag to the latest commit
+print(f"Creating tag '{tag}'...")
+api.create_tag(
+    repo_id=hf_hub_repo,
+    tag=tag,
+    repo_type="model",
+    exist_ok=True  # prevents error if the tag already exists
+)
+print(f"Successfully created tag '{tag}' in repo '{hf_hub_repo}'.")
