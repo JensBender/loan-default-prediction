77346159a46c7d44760a584463132f656baa5113
Jens Bender <jens.bender.001@gmail.com>
2025-11-05 09:10:41 +0800

    In upload_model_to_huggingface.py: Add constants

--- upload_model_to_huggingface.py
+++ upload_model_to_huggingface.py
@@ -1,32 +1,30 @@
 from huggingface_hub import HfApi
 
-api = HfApi()
+# Constants
+LOCAL_PIPELINE_PATH = "models/loan_default_rf_pipeline.joblib"  
+HF_HUB_REPO = "JensBender/loan-default-prediction-pipeline"  # make sure to create Hugging Face Hub repository first
+FILENAME = "loan_default_rf_pipeline.joblib"
+TAG = "v1.0"  # version tag
 
-# Path to model pipeline on local machine
-pipeline_path = "models/loan_default_rf_pipeline.joblib"
-# Name of Hugging Face Hub repository (make sure to create repo first)
-hf_hub_repo = "JensBender/loan-default-prediction-pipeline"
-# Filename of model pipeline in the Hugging Face Hub repository
-filename = "loan_default_rf_pipeline.joblib"
-# Version tag
-tag = "v1.0"
+# Initialize Hugging Face API client 
+api = HfApi()
 
 # Upload pipeline
 print("Uploading pipeline to Hugging Face Hub...")
 api.upload_file(
-    path_or_fileobj=pipeline_path,
-    repo_id=hf_hub_repo,
-    path_in_repo=filename,
+    path_or_fileobj=LOCAL_PIPELINE_PATH,
+    repo_id=HF_HUB_REPO,
+    path_in_repo=FILENAME,
     repo_type="model"
 )
-print(f"Successfully uploaded pipeline '{filename}' to Hugging Face Hub repository '{hf_hub_repo}'.")
+print(f"Successfully uploaded pipeline '{FILENAME}' to Hugging Face Hub repository '{HF_HUB_REPO}'.")
 
 # Add the version tag to the latest commit
-print(f"Creating tag '{tag}'...")
+print(f"Creating tag '{TAG}'...")
 api.create_tag(
-    repo_id=hf_hub_repo,
-    tag=tag,
+    repo_id=HF_HUB_REPO,
+    tag=TAG,
     repo_type="model",
     exist_ok=True  # prevents error if the tag already exists
 )
-print(f"Successfully created tag '{tag}' in repo '{hf_hub_repo}'.")
+print(f"Successfully created tag '{TAG}' in repo '{HF_HUB_REPO}'.")
