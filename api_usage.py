65ecf1d473b8001eef9e2ca8a26cad1d998d03ec
Jens Bender <jens.bender.001@gmail.com>
2025-11-10 11:01:41 +0800

    Add api_usage.py with example API usage code

---
+++ api_usage.py
@@ -0,0 +1,31 @@
+import requests 
+
+# Create example applicant data (JSON payload)
+applicant_data = {
+    "income": 300000,
+    "age": 30,
+    "experience": 3,
+    "married": "single",
+    "house_ownership": "rented",
+    "car_ownership": "no",
+    "profession": "artist",
+    "city": "sikar",
+    "state": "rajasthan",
+    "current_job_yrs": 3,
+    "current_house_yrs": 11,
+}
+
+# API post request to the predict endpoint of the FastAPI backend on Hugging Face Spaces
+response = requests.post("https://jensbender-loan-default-prediction-app.hf.space/predict", json=applicant_data)
+
+# Check if request was successful
+response.raise_for_status()
+
+# Extract prediction and predicted probability of default
+prediction_response = response.json()
+prediction_result = prediction_response["results"][0]
+prediction = prediction_result["prediction"]
+default_probability = prediction_result["probabilities"]["Default"]
+
+print(f"Probability of default: {default_probability:.2f}")
+print(f"Prediction: {prediction}")
